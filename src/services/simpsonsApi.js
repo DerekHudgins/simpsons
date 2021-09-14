2ecff89f30aaa2dd44516413fe7db922aab42648
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-14 16:31:02 -0700

    created fetchQuote service

---
+++ src/services/simpsonsApi.js
@@ -0,0 +1,10 @@
+export const fetchQuote = async () => {
+  const res = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+  const [quote] = await res.json();
+
+  return {
+    text: quote.quote,
+    name: quote.character,
+    image: quote.image,
+  };
+};
